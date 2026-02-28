50a260b7547506b35076aa41999c90f95255e6bf
RcNryGin <RcNryGin@gmail.com>
2026-02-28 20:12:00 +0800

    implement Student class

---
+++ src/student.cpp
@@ -0,0 +1,21 @@
+#include"student.h"
+#include<iostream>
+
+Student::Student()
+{
+    id=0;
+    name="";
+    age=0;
+    score=0.0;
+}
+Student::Student(int32_t i, string n, int a, double s)
+{
+    id=i;
+    name=n;
+    age=a;
+    score=s;
+}
+void Student::disp()
+{
+    cout<<id<<" "<<name<<" "<<age<<" "<<score<<endl;
+}
---
+++ src/student.h
@@ -0,0 +1,19 @@
+#ifndef STUDENT_H
+#define STUDENT_H
+
+#include<cstdint>
+#include<string>
+using namespace std;
+
+class Student
+{
+public:
+    Student();
+    Student(int32_t id, string name, int age, double score);
+    void disp();
+    int32_t id;
+    string name;
+    int age;
+    double score;
+};
+#endif
